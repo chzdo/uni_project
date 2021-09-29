fd951b40f70516844e83634af552ed56c795ad4e
Stanley <chido.nduaguibe@gmail.com>
2021-09-29 21:00:23 +0100

    added populate to controllers

--- src/controller/__config.ts
+++ src/controller/__config.ts
@@ -4,38 +4,38 @@ import mongoose from "mongoose";
 import { processQueryOptions, processUpdateOptions } from "./__query";
 
 async function create(body, model: mongoose.Model<any>): Promise<mongoose.Model<any>> {
- const id = (await count({}, model)) + 1;
- return await model.create({ ...body, id });
+  const id = (await count({}, model)) + 1;
+  return await model.create({ ...body, id, _id: id });
 }
 
 async function count(query, model?: mongoose.Model<any>): Promise<number> {
- return await model.countDocuments({ ...query });
+  return await model.countDocuments({ ...query });
 }
-async function findOne(model: mongoose.Model<any>, options: options): Promise<Record<string, any>> {
- const { query, project } = processQueryOptions(options);
- return await model.findOne(query).select(project);
+async function findOne(model: mongoose.Model<any>, options: options, populate?: { path: string, select?: string, match?: any }): Promise<Record<string, any>> {
+  const { query, project } = processQueryOptions(options);
+  return await model.findOne(query).select(project).populate(populate);
 }
 
-async function find(model: mongoose.Model<any>, options: options): Promise<Record<string, any>> {
- const { query, limit, skip, sort, project } = processQueryOptions(options);
- return await model.find(query).select(project).sort(sort).skip(skip).limit(limit);
+async function find(model: mongoose.Model<any>, options: options, populate?: { path: string, select?: string, match?: any }): Promise<Record<string, any>> {
+  const { query, limit, skip, sort, project } = processQueryOptions(options);
+  return await model.find(query).select(project).populate(populate).sort(sort).skip(skip).limit(limit);
 }
 
 async function update(model: mongoose.Model<any>, options: options): Promise<Record<string, any>> {
- const { query, body } = processUpdateOptions(options);
- return await model.updateOne(query, body);
+  const { query, body } = processUpdateOptions(options);
+  return await model.updateOne(query, body);
 }
 
 async function hardDelete(model: mongoose.Model<any>, options: options): Promise<Record<string, any>> {
- const { query, body } = processUpdateOptions(options);
- return await model.deleteOne(query);
+  const { query, body } = processUpdateOptions(options);
+  return await model.deleteOne(query);
 }
 
 async function softDelete(model: mongoose.Model<any>, options: options): Promise<Record<string, any>> {
- const { query, body } = processUpdateOptions(options);
- return await model.updateOne(query, {
-  isDeleted: true,
- });
+  const { query, body } = processUpdateOptions(options);
+  return await model.updateOne(query, {
+    isDeleted: true,
+  });
 }
 
 export { create, count, findOne, find, update, hardDelete, softDelete };
