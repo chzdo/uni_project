a2b4c6d3cb862ec831e1dd929f91af087d06515a
Stanley <chido.nduaguibe@gmail.com>
2021-10-05 14:28:50 +0100

    more fixes

--- src/controller/__config.ts
+++ src/controller/__config.ts
@@ -4,39 +4,49 @@ import mongoose from "mongoose";
 import { processQueryOptions, processUpdateOptions } from "./__query";
 
 async function create(body, model: mongoose.Model<any>): Promise<mongoose.Model<any>> {
- let { id } = (await model.findOne().sort([["_id", "DESC"]])).toJSON();
+ const lastItem = await model.findOne().sort([["_id", "DESC"]]);
+
+ let { id } = lastItem ? lastItem.toJSON() : { id: 0 };
  id++;
-  return await model.create({ ...body, id, _id: id });
+ return await model.create({ ...body, id, _id: id });
 }
 
 async function count(query, model?: mongoose.Model<any>): Promise<number> {
-  return await model.countDocuments({ ...query });
+ return await model.countDocuments({ ...query });
 }
-async function findOne(model: mongoose.Model<any>, options: options, populate?: { path: string, select?: string, match?: any }): Promise<Record<string, any>> {
-  const { query, project } = processQueryOptions(options);
-  return await model.findOne(query).select(project).populate(populate);
+async function findOne(
+ model: mongoose.Model<any>,
+ options: options,
+ populate?: { path: string; select?: string; match?: any }
+): Promise<Record<string, any>> {
+ const { query, project } = processQueryOptions(options);
+ return await model.findOne(query).select(project).populate(populate);
 }
 
-async function find(model: mongoose.Model<any>, options: options, populate?: { path: string, select?: string, match?: any }): Promise<Record<string, any>> {
-  const { query, limit, skip, sort, project } = processQueryOptions(options);
-  return await model.find(query).select(project).populate(populate).sort(sort).skip(skip).limit(limit);
+async function find(
+ model: mongoose.Model<any>,
+ options: options,
+ populate?: { path: string; select?: string; match?: any }
+): Promise<Record<string, any>> {
+ const { query, limit, skip, sort, project } = processQueryOptions(options);
+ return await model.find(query).select(project).populate(populate).sort(sort).skip(skip).limit(limit);
 }
 
 async function update(model: mongoose.Model<any>, options: options): Promise<Record<string, any>> {
-  const { query, body } = processUpdateOptions(options);
-  return await model.updateOne(query, body);
+ const { query, body } = processUpdateOptions(options);
+ return await model.updateOne(query, body);
 }
 
 async function hardDelete(model: mongoose.Model<any>, options: options): Promise<Record<string, any>> {
-  const { query, body } = processUpdateOptions(options);
-  return await model.deleteOne(query);
+ const { query, body } = processUpdateOptions(options);
+ return await model.deleteOne(query);
 }
 
 async function softDelete(model: mongoose.Model<any>, options: options): Promise<Record<string, any>> {
-  const { query, body } = processUpdateOptions(options);
-  return await model.updateOne(query, {
-    isDeleted: true,
-  });
+ const { query, body } = processUpdateOptions(options);
+ return await model.updateOne(query, {
+  isDeleted: true,
+ });
 }
 
 export { create, count, findOne, find, update, hardDelete, softDelete };
--- utils/packages.ts
+++ utils/packages.ts
@@ -1,37 +1,35 @@
-
 import axios from "axios";
 import { logger } from "./winston";
 
-
 async function makeAPICall(method: any, url: string, headers?: Record<any, any>, data?: Record<any, any>) {
-  try {
-    const response = await axios.request({
-      url,
-      method,
-      data,
-      headers,
-    });
+ try {
+  const response = await axios.request({
+   url,
+   method,
+   data,
+   headers,
+  });
 
-    return response.data;
-  } catch (error: any) {
-    const { response } = error;
+  return response.data;
+ } catch (error: any) {
+  const { response } = error;
 
-    if (response && typeof response?.data == "object") {
-      const { statusCode, message } = response.data;
-      logger.error(message);
-      return {
-        status: statusCode,
-        message,
-      };
-    }
-
-    const { message, status } = error.toJSON();
-    logger.error(message);
-    return {
-      status,
-      message,
-    };
+  if (response && typeof response?.data == "object") {
+   const { statusCode, message } = response.data;
+   logger.error(message);
+   return {
+    status: statusCode,
+    message,
+   };
   }
+
+  const { message, status } = error.toJSON();
+  logger.error(message);
+  return {
+   status,
+   message,
+  };
+ }
 }
 
 export { makeAPICall };
